a2e5461c3757d297846ddbffc5f203669b69548f
choign3123 <choign3123@naver.com>
2025-11-30 14:42:43 +0900

    섹션3: DataSource 예제

---
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,66 @@
+package hello.jdbc.connection;
+
+import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.connction.ConnectionConst;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(ConnectionConst.URL, ConnectionConst.USERNAME, ConnectionConst.PASSWORD);
+        Connection con2 = DriverManager.getConnection(ConnectionConst.URL, ConnectionConst.USERNAME, ConnectionConst.PASSWORD);
+
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager () throws SQLException {
+        // DriverManagerDataSource (spring에서 제공해주는 DataSource를 구현한 DriverManager) - 항상 새로운 커넥션을 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(ConnectionConst.URL, ConnectionConst.USERNAME, ConnectionConst.PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        // 아마도 org.springframework.boot:spring-boot-starter-jdbc 얘가 HikariDataSource 를 추가해준 것 같음
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(ConnectionConst.URL);
+        dataSource.setUsername(ConnectionConst.USERNAME);
+        dataSource.setPassword(ConnectionConst.PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        log.info("getConnection() start");
+        Connection con1 = dataSource.getConnection();
+        log.info("connection={}, class={}", con1, con1.getClass());
+
+        Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}", con2, con2.getClass());
+
+//        Connection con3 = dataSource.getConnection();
+//        Connection con4 = dataSource.getConnection();
+//        Connection con5 = dataSource.getConnection();
+//        Connection con6 = dataSource.getConnection();
+//        Connection con7 = dataSource.getConnection();
+//        Connection con8 = dataSource.getConnection();
+//        Connection con9 = dataSource.getConnection();
+//        Connection con10 = dataSource.getConnection();
+//        Connection con11 = dataSource.getConnection(); // waiting 타임아웃으로 종료됨
+    }
+}
