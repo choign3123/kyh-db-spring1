f383047a3e7cb7befbdf1e0da306c127a3808858
GANA <choign3123@naver.com>
2025-12-22 21:51:47 +0900

    세션7: 체크 예외와 인터페이스

---
+++ src/main/java/hello/jdbc/repository/MemberRepositoryEx.java
@@ -0,0 +1,14 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+
+import java.sql.SQLException;
+
+// (throws SQLException 때문에) JDBC 기술에 종속적인 인터페이스
+public interface MemberRepositoryEx {
+
+    Member save(Member member) throws SQLException;
+    Member findById(String memberId) throws SQLException;
+    void update(String memberId, int money) throws SQLException;
+    void delete(String memberId) throws SQLException;
+}
