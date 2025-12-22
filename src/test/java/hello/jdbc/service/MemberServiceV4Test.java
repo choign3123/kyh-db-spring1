08bf97adaa5c7ad0ac8ca0b22e494b06c3333e79
GANA <choign3123@naver.com>
2025-12-22 22:03:37 +0900

    세션7: 런타임 예외 적용

---
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -0,0 +1,123 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepository;
+import hello.jdbc.repository.MemberRepositoryV3;
+import hello.jdbc.repository.MemberRepositoryV4_1;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * 예외 누수 문제 해결
+ * SQLException 제거
+ *
+ * MemberRepository 인터페이스에 의존
+ */
+@Slf4j
+@SpringBootTest
+class MemberServiceV4Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    @Autowired
+    private MemberServiceV4 memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @TestConfiguration
+    static class TestConfig {
+
+        private final DataSource dataSource;
+
+        public TestConfig(DataSource dataSource) {
+            this.dataSource = dataSource;
+        }
+
+        @Bean
+        MemberRepository memberRepository() {
+            return new MemberRepositoryV4_1(dataSource);
+        }
+
+        @Bean
+        MemberServiceV4 memberServiceV4() {
+            return new MemberServiceV4(memberRepository());
+        }
+    }
+
+    @AfterEach
+    void after() {
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
+    }
+
+    @Test
+    void AopCheck() {
+        log.info("memberService class={}", memberService.getClass());
+        log.info("memberRepository class={}", memberRepository.getClass());
+        Assertions.assertThat(AopUtils.isAopProxy(memberRepository)).isFalse();
+        Assertions.assertThat(AopUtils.isAopProxy(memberService)).isTrue();
+    }
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10_000);
+        Member memberB = new Member(MEMBER_B, 10_000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        // when
+        log.debug("트랜잭션 시작");
+        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+        log.debug("트랜잭션 종료");
+
+        // then
+        Member updatedA = memberRepository.findById(memberA.getMemberId());
+        assertEquals(8000, updatedA.getMoney());
+        Member updatedB = memberRepository.findById(memberB.getMemberId());
+        assertEquals(12000, updatedB.getMoney());
+    }
+
+    @Test
+    @DisplayName("이체 중 예외 발생")
+    void accountTransferEx() throws SQLException {
+        // given
+        Member memberA = new Member(MEMBER_A, 10_000);
+        Member memberEx = new Member(MEMBER_EX, 10_000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberEx);
+
+        // when
+        log.debug("트랜잭션 시작");
+        assertThrows(
+                IllegalStateException.class,
+                () -> {memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000);}
+        );
+        log.debug("트랜잭션 종료");
+
+        // then
+        Member updatedA = memberRepository.findById(memberA.getMemberId());
+        assertEquals(10_000, updatedA.getMoney());
+        Member updatedB = memberRepository.findById(memberEx.getMemberId());
+        assertEquals(10_000, updatedB.getMoney());
+    }
+}
